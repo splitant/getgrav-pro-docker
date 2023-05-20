8e7f8b2b2de6056e0de535e4397b13b062e04d5c
Axel <a.depret@choosit.com>
2023-05-20 19:33:18 +0200

    Add Xdebug + get grav admin + some others changes

--- .docker/getgrav/scripts/docker_init.sh
+++ .docker/getgrav/scripts/docker_init.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-if [ $GRAV_SETUP = 1 ]; then
+if [ $GRAV_SETUP = 1 ] && [ -z "$(ls -A .)" ]; then
     if [ $GRAV_VERSION = "latest" ]; then
         runuser -g www-data -u www-data -- composer create-project getgrav/grav .
     else
         runuser -g www-data -u www-data -- composer create-project getgrav/grav . $GRAV_VERSION
     fi
+
+    # Install Grav Standard Administration Panel Plugin
+    runuser -g www-data -u www-data -- bin/gpm install admin
 fi
 
 exec sh -c "cron && apache2-foreground"
